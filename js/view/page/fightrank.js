a5585bd875e553650a31f1447e6103fa91575f76
McFog <mcfogw@gmail.com>
2014-02-11 20:08:04 +0800

    up

--- js/view/page/fightrank.js
+++ js/view/page/fightrank.js
@@ -1,9 +1,42 @@
 (function(){
-  define(['view/base', 'app'], function(Base, app){
+  define(['view/base', 'app', 'bootbox'], function(Base, app, bootbox){
     return Base.extend({
       template: 'page/fightrank',
       events: {
-        'click .btn-fight': 'onFight'
+        'click .btn-fight': 'onFight',
+        'click .btn-attack': 'onAttack'
+      },
+      initialize: function(){
+        Base.prototype.initialize.apply(this, arguments);
+        this.data.competitor = {};
+        return this.getRankCompetitors();
+      },
+      getRankCompetitors: function(){
+        var this$ = this;
+        return app.me.request('arena', 'GetRankCompetitors').then(function(it){
+          this$.data.competitor = it;
+          return this$.renderFields('', 'competitor');
+        });
+      },
+      onAttack: function(event){
+        var CompetitorRank, this$ = this;
+        if (this.data.fighting) {
+          return;
+        }
+        CompetitorRank = parseInt(this.$(event.currentTarget).attr('rank'));
+        return app.me.request('arena', 'RankFight', {
+          CompetitorRank: CompetitorRank
+        }).then(function(it){
+          this$.getRankCompetitors();
+          if (it.Win === 1) {
+            return bootbox.alert("胜利");
+          } else {
+            return bootbox.alert("失败");
+          }
+        }, function(it){
+          var ref$;
+          return bootbox.alert(((ref$ = it.content) != null ? ref$.message : void 8) || '连接失败');
+        });
       },
       onFight: function(event){
         var $target, times, this$ = this;
